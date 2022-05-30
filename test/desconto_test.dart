cf6717bc38d1c495c21c9e9fb7df1cb74d137559
erisvaldocorreia2 <erisvaldo.correia@rethink.dev>
2022-05-30 09:34:49 -0300

    chore: adicionando novo teste para funcao e agrupando testes

--- test/desconto_test.dart
+++ test/desconto_test.dart
@@ -2,12 +2,23 @@ import 'package:flutter_test/flutter_test.dart';
 import 'package:unit_test_flutter_estudo/desconto.dart';
 
 void main() {
-  test('Testando a função de calcular desconto com inteiro', () {
-    const inicial = 150.0;
-    const desconto = 10.0;
+  group('Testando função calculaDesconto', () {
+    test('Testando a função de calcular desconto com inteiro', () {
+      const inicial = 150.0;
+      const desconto = 10.0;
 
-    double retorno = calculaDesconto(inicial, desconto, 0);
+      double retorno = calculaDesconto(inicial, desconto, 0);
 
-    expect(retorno, 140.0);
+      expect(retorno, 140.0);
+    });
+
+    test('Testando a função de calcular desconto com percentual', () {
+      const inicial = 150.0;
+      const desconto = 10.0;
+
+      double retorno = calculaDesconto(inicial, desconto, 1);
+
+      expect(retorno, 135.0);
+    });
   });
 }
